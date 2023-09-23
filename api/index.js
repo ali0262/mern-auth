2619cc71fcc81c1ea297081783c6561ed548cf38
ali p <alifront2233@gmail.com>
2023-09-23 09:54:45 +0330

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import connectDB from "./db/connection.js";
 
+dotenv.config({ path: ".env" });
 const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`server is running on port :${port}`);
-});
+const start = async () => {
+  try {
+    const conn = await connectDB(process.env.MONGODB_URL);
+    console.log(
+      `successfully connected on DATABASE on ${conn.connection.host}`.bgCyan
+    );
+
+    app.listen(port, () => {
+      console.log(`server is running on port : ${port}`.bgMagenta);
+    });
+  } catch (err) {
+    console.log(`${err.message}`.bgRed);
+  }
+};
+
+start();
