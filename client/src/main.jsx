4d306121a33653c407caf576003bb4c239df867e
ali p <alifront2233@gmail.com>
2023-09-29 09:40:23 +0330

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import { render } from "preact";
 import App from "./app.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("app")
 );
